7aac0bde58d334990f2c3dee4078d61e8d9f3950
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2025-01-28 21:06:01 +0300

    fix: build gradle module1 stage2 v.3

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,21 @@
 package com.epam.demo;
 
 import java.util.List;
+import com.epam.utils.StringUtils;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-       //magic happens here
+        //magic happens here
+        StringUtils obj = new StringUtils();
+        for (String str : args) {
+            if(!obj.isPositiveNumber(str))
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        List<String> list =  List.of("1.22", "0", "33", "87");
+        System.out.println(isAllPositiveNumbers(list));
     }
 }
